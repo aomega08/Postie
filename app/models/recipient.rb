35e27b355e26305f76bee9e8e68e1c9ee1e6f1f6
Francesco Boffa <fra.boffa@gmail.com>
2018-04-02 21:51:32 +0100

    ✨ Validate email format with RFC 5322 regular expression

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -2,8 +2,18 @@
 
 # A recipient represents a single email address. A name can be attached.
 class Recipient < ApplicationRecord
+  RFC_5322 = %r{
+    ^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"
+    (?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")
+    @
+    (?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|
+    \[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}
+    (?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|
+    [a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$
+  }x
+
   has_many :message_recipients, dependent: :delete_all
   has_many :messages, through: :message_recipients
 
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: RFC_5322 }
 end
--- spec/models/recipient_spec.rb
+++ spec/models/recipient_spec.rb
@@ -5,4 +5,8 @@ require 'rails_helper'
 RSpec.describe Recipient, type: :model do
   it { is_expected.to have_many(:messages) }
   it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to allow_value('test@example.com').for(:email) }
+  it { is_expected.to allow_value('test@127.0.0.1').for(:email) }
+  it { is_expected.to allow_value('test+alias@127.0.0.1').for(:email) }
+  it { is_expected.to_not allow_value('test').for(:email) }
 end
