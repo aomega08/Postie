e4156719e2b8636846932c4afaeaea4d1d317cce
Francesco Boffa <fra.boffa@gmail.com>
2018-04-03 22:10:06 +0100

    🔧 Only log warn and fatal during specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,6 +15,8 @@ Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+Rails.logger.level = :warn
+
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
